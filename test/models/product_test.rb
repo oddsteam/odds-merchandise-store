7e7d4ca85dd049816ae704d0c09dc9a49dda51c5
zkan <zkan.cs@gmail.com>
2024-04-07 17:21:55 +0700

    Check valid product and ensure name must be present + ignore *.swp

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid product" do
+    product = Product.new(
+      name: "ODDS Whole Bean Coffee",
+      description: "Perfect start to your day",
+      price: 25.00,
+    )
+    assert product.valid?
+  end
+
+  test "should not save product without name" do
+    product = Product.new
+    assert_not product.save
+  end
 end
